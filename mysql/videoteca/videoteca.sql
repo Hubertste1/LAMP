22c8d370d3f22b362d6e44c75e7204dc1be55c83
Hubertste1 <hubertstefanski85@gmail.com>
2024-12-03 12:24:54 +0000

    ,

--- mysql/videoteca/videoteca.sql
+++ mysql/videoteca/videoteca.sql
@@ -1,6 +1,8 @@
+-- Creazione del database
 CREATE DATABASE IF NOT EXISTS videoteca;
 USE videoteca;
 
+-- Creazione della tabella Regista
 CREATE TABLE Regista (
     ID_Regista INT PRIMARY KEY,
     Nome VARCHAR(50),
@@ -8,6 +10,7 @@ CREATE TABLE Regista (
     Data_di_nascita DATE
 );
 
+-- Creazione della tabella Attore
 CREATE TABLE Attore (
     ID_Attore INT PRIMARY KEY,
     Nome VARCHAR(50),
@@ -15,6 +18,7 @@ CREATE TABLE Attore (
     Data_di_nascita DATE
 );
 
+-- Creazione della tabella Produttore
 CREATE TABLE Produttore (
     ID_Produttore INT PRIMARY KEY,
     Nome VARCHAR(50),
@@ -22,11 +26,13 @@ CREATE TABLE Produttore (
     Data_di_nascita DATE
 );
 
+-- Creazione della tabella Categoria
 CREATE TABLE Categoria (
     ID_Categoria INT PRIMARY KEY,
     Nome_categoria VARCHAR(50)
 );
 
+-- Creazione della tabella Film
 CREATE TABLE Film (
     ID_Film INT PRIMARY KEY,
     ID_Regista INT,
@@ -37,6 +43,7 @@ CREATE TABLE Film (
     FOREIGN KEY (ID_Categoria) REFERENCES Categoria(ID_Categoria)
 );
 
+-- Creazione della tabella Film_Attore
 CREATE TABLE Film_Attore (
     ID_Film INT,
     ID_Attore INT,
@@ -45,12 +52,14 @@ CREATE TABLE Film_Attore (
     FOREIGN KEY (ID_Attore) REFERENCES Attore(ID_Attore)
 );
 
+-- Creazione della tabella Cliente
 CREATE TABLE Cliente (
     ID_Cliente INT PRIMARY KEY,
     Nome VARCHAR(50),
     Cognome VARCHAR(50)
 );
 
+-- Creazione della tabella Acquisto
 CREATE TABLE Acquisto (
     ID_Acquisto INT PRIMARY KEY,
     ID_Film INT,
@@ -59,7 +68,6 @@ CREATE TABLE Acquisto (
     FOREIGN KEY (ID_Cliente) REFERENCES Cliente(ID_Cliente)
 );
 
-
 -- Inserimento dati nella tabella Regista
 INSERT INTO Regista (ID_Regista, Nome, Cognome, Data_di_nascita)
 VALUES
@@ -67,10 +75,11 @@ VALUES
 (2, 'Quentin', 'Tarantino', '1963-03-27');
 
 -- Inserimento dati nella tabella Attore
-INSERT INTO Regista (ID_Regista, Nome, Cognome, Data_di_nascita)
+INSERT INTO Attore (ID_Attore, Nome, Cognome, Data_di_nascita)
 VALUES
-(1, 'Christopher', 'Nolan', '1970-07-30'),
-(2, 'Quentin', 'Tarantino', '1963-03-27');
+(1, 'Leonardo', 'DiCaprio', '1974-11-11'),
+(2, 'Joseph', 'Gordon-Levitt', '1981-02-17'),
+(3, 'Samuel', 'L. Jackson', '1948-12-21');
 
 -- Inserimento dati nella tabella Produttore
 INSERT INTO Produttore (ID_Produttore, Nome, Cognome, Data_di_nascita)
@@ -109,4 +118,3 @@ INSERT INTO Acquisto (ID_Acquisto, ID_Film, ID_Cliente)
 VALUES
 (1, 1, 1),
 (2, 2, 2);
-
