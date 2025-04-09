24058feecfbb06f40409ecb3a3426bdf0ec0ed42
Hubertste1 <hubertstefanski85@gmail.com>
2025-04-09 11:31:03 +0000

    .

--- www/es-05/login.php
+++ www/es-05/login.php
@@ -7,51 +7,48 @@ session_start();
 
 $msg = $_GET['error'] ?? '';
 
-if(isset($_SESSION['username'])) {
+if (isset($_SESSION['username'])) {
     $msg = 'Login già effettuato';
-}
-else if($_SERVER['REQUEST_METHOD'] == 'POST') {
+} elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'] ?? '';
     $password = $_POST['password'] ?? '';
-  try{
-    [$loginRetval, $loginRetmsg] = login($username, $password);
-    
-    $msg = $loginRetmsg;
-    
-    if($loginRetval) {
-        $_SESSION['username'] = $username; 
+    try {
+        [$loginRetval, $loginRetmsg] = login($username, $password);
 
-        $link = 'Location: ';
-        $link .= $_POST['from'] != null ? $_POST['from'] : 'index.php';
+        $msg = $loginRetmsg;
 
-        header($link);
-        die();
+        if ($loginRetval) {
+            $_SESSION['username'] = $username;
 
+            $link = 'Location: ';
+            // Controlliamo se "from" è definito, altrimenti diamo il valore di default "index.php"
+            $link .= isset($_POST['from']) && $_POST['from'] ? $_POST['from'] : 'index.php';
+
+            header($link);
+            exit();
+        }
+    } catch (Exception $e) {
+        $msg = 'Errore durante il login: ' . $e->getMessage();
     }
-  }catch(Exception $e){
-    $msg = 'Errore durante il login: '. $e->getMessage();
-  }
 }
-?>
 
-<?php
 //Form di login
 $html_form = <<<FORM
 <form action="$_SERVER[PHP_SELF]" method="post">
   <label for="nome"> </label><input type="text" name="username" placeholder="Nome utente" required/><br />
   <label for="password"> </label><input type="password" name="password" placeholder="Password" required/><br />
   <input type="submit" value="Login" /><input type="reset" value="Cancel" />
-  <input type="hidden" name="from" value="{$_GET['from']}" />
+  <input type="hidden" name="from" value="{$_GET['from'] ?? ''}" />
   <p class='error'>$msg</p>
 </form>
 FORM;
 
 // Creo il codice html da visualizzare a seconda dei valori di $from e $retval
-  $html_out = "<p class='error'>$errmsg</p>";
-  $html_out .= $html_form;
-  $html_out .= "Non hai un account? <a href='register.php'>Registrati adesso</a>.<br />";
-  $html_out .= "Hai dimenticato la password? <a href='pwd_reset.php'>Resetta la password</a>.<br />";
-  $html_out .= "<a href='index.php'>Torna alla Home Page</a>.<br />";
+$html_out = "<p class='error'>$msg</p>";
+$html_out .= $html_form;
+$html_out .= "Non hai un account? <a href='register.php'>Registrati adesso</a>.<br />";
+$html_out .= "Hai dimenticato la password? <a href='pwd_reset.php'>Resetta la password</a>.<br />";
+$html_out .= "<a href='index.php'>Torna alla Home Page</a>.<br />";
 ?>
 <!DOCTYPE html>
 <html>
