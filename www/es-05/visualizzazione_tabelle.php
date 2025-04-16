67faccff7b45379e6fe8f1283e232ee26f69f2b2
Hubertste1 <hubertstefanski85@gmail.com>
2025-04-16 13:58:49 +0000

    .

--- www/es-05/visualizzazione_tabelle.php
+++ www/es-05/visualizzazione_tabelle.php
@@ -5,18 +5,18 @@ define('DB_USERNAME', 'ES05_user');
 define('DB_PASSWORD', 'mia_password');
 define('DB_NAME', 'ES05');
 
-$conn = mysqli_connect($host, $user, $password, $database);
+$conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME);
 
-// Controllo della connessione
+
 if (!$conn) {
     die("Connessione fallita: " . mysqli_connect_error());
 }
 
-// Query per ottenere le tabelle
+
 $query = "SHOW TABLES FROM $database";
 $result = mysqli_query($conn, $query);
 
-// Visualizzazione delle tabelle
+
 if ($result) {
     echo "<h3>Tabelle presenti nel database <em>$database</em>:</h3>";
     echo "<ul>";
@@ -28,6 +28,6 @@ if ($result) {
     echo "Errore nella query: " . mysqli_error($conn);
 }
 
-// Chiusura della connessione
+
 mysqli_close($conn);
 ?>
