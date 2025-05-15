7cce3d09c03496beaf346e205a8c2081104350c0
Hubertste1 <hubertstefanski85@gmail.com>
2025-05-15 07:29:13 +0000

    .

---
+++ www/es-06/sregistrazione.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+$username=$_SESSION["username"];
+$html_link1 = "<a href='login.php'>Login</a>";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login dopo registrazione</title>
+</head>
+<body>
+    <h2>Benvenuto <?php $username?>, la registrazione e' andata a buon fine</h2>
+    <?php $html_link1?>
+</body>
+</html>
