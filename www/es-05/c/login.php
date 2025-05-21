91aacc19dc8b439f09c913396fa4ab54331c65f2
Hubertste1 <hubertstefanski85@gmail.com>
2025-05-21 13:22:19 +0000

    .

--- www/es-05/c/login.php
+++ www/es-05/c/login.php
@@ -25,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     if (mysqli_num_rows($risultato) > 0) {
         $_SESSION['username'] = $username;
-        echo "Benvenuto login andato a buon fine"
+        echo "Benvenuto login andato a buon fine";
     } else {
         echo "Nome utente o password errati.";
     }
